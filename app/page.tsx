73e8cb5c95da3c0a67807f358da6ee384934ce91
a3chron <121883800+kurtschambach@users.noreply.github.com>
2025-04-04 18:31:37 +0200

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-white text-black">
+      <h1 className="text-xl">NextJS Template</h1>
+    </main>
+  );
+}
